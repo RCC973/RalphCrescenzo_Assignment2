f0f30b5f801dcd6abf7f1db3ca8aa15897eac504
rcc973 <1pringlE>
2016-10-12 04:17:22 -0500

    update

--- controllers/main.js
+++ controllers/main.js
@@ -3,5 +3,17 @@
  */
 /* GET home page */
 module.exports.index = function(req, res){
-    res.render('index', { title: 'Express'});
+    res.render('index', { title: 'Home'});
+};
+/* GET list page */
+module.exports.list = function(req, res){
+    res.render('list', { title: 'Note List'});
+};
+/* GET editor page */
+module.exports.editor = function(req, res){
+    res.render('editor', { title: 'Note Editor'});
+};
+/* GET defaultNote page */
+module.exports.defaultNote = function(req, res){
+    res.render('defaultNote', { title: 'Default'});
 };
