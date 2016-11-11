734c472b53f313b17fae7b8189f708252f562869
RCC973 <ralphcrescenzo2016@u.northwestern.edu>
2016-11-10 22:02:38 -0600

    Add files via upload

--- controllers/main.js
+++ controllers/main.js
@@ -4,16 +4,20 @@
 /* GET home page */
 module.exports.index = function(req, res){
     res.render('index', { title: 'Home'});
+    console.log('index page called');
 };
 /* GET list page */
 module.exports.list = function(req, res){
     res.render('list', { title: 'Note List'});
+    console.log('list page called');
 };
 /* GET editor page */
 module.exports.editor = function(req, res){
     res.render('editor', { title: 'Note Editor'});
+    console.log('editor page called');
 };
 /* GET defaultNote page */
 module.exports.defaultNote = function(req, res){
     res.render('defaultNote', { title: 'Default'});
+    console.log('default page called');
 };
